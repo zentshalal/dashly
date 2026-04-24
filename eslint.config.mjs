c81c7e6861eb72605349f2f996fb01d18e7b9b4b
zents <kyliandevhalal@gmail.com>
2026-04-24 21:06:35 +0200

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,11 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "no-unused-vars": false,
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
